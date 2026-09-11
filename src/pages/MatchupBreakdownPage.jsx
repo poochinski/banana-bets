49b3f9afc33301aa1330b91c03f6d06d7a6fc04a
poochinski <editfilminc@gmail.com>
2026-09-11 00:11:05 -0700

    Compose integrated matchup research and helmet asset bridge

--- src/pages/MatchupBreakdownPage.jsx
+++ src/pages/MatchupBreakdownPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MatchupRetroPage from "./MatchupRetroPage";
 import MatchupApiV2Insights from "./MatchupApiV2Insights";
+import MatchupHelmetAssetBridge from "./MatchupHelmetAssetBridge";
 
 export default function MatchupBreakdownPage(props) {
   return (
     <>
       <MatchupRetroPage {...props} />
+      <MatchupHelmetAssetBridge {...props} />
       <MatchupApiV2Insights {...props} />
     </>
   );
