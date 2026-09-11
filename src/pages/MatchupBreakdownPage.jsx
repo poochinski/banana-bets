39d0358f35db57a3973c66463a906b28722efdb4
poochinski <editfilminc@gmail.com>
2026-09-11 00:13:01 -0700

    Integrate matchup research flow and individual helmet assets
    * Integrate API v2 research panels into matchup flow
    
    * Compose integrated matchup research and helmet asset bridge
    
    * Style integrated matchup research flow
    
    * Load integrated matchup research styles
    
    * Keep helmet sprite fallback aligned when matchups change

--- src/pages/MatchupBreakdownPage.jsx
+++ src/pages/MatchupBreakdownPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MatchupRetroPage from "./MatchupRetroPage";
 import MatchupApiV2Insights from "./MatchupApiV2Insights";
+import MatchupHelmetAssetBridge from "./MatchupHelmetAssetBridge";
+import "./MatchupIntegratedFlow.css";
 
 export default function MatchupBreakdownPage(props) {
   return (
     <>
       <MatchupRetroPage {...props} />
+      <MatchupHelmetAssetBridge {...props} />
       <MatchupApiV2Insights {...props} />
     </>
   );
