9b45aca531378f7ad06722e5f393d5c67345eb01
poochinski <editfilminc@gmail.com>
2026-09-10 19:49:56 -0700

    Load monochrome bet-slip logo override

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./pages/BetBuilderReceipt.css";
+import "./pages/BetBuilderReceiptLogo.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
